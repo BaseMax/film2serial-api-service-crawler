13ab4acaaed61c98b9721f0ef3c445c3a4849fda
Max Base <MaxBaseCode@Gmail.Com>
2020-11-06 17:48:17 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,9 @@
-
+<?php
+if(isset($db)) {
+  $db->connect("localhost", "movie_db", 'df*gdf*gd*ff*gd*ff*gd*fg*dfg*');
+  $db->db="movie_db";
+  $db->create_database($db->db, false);
+}
+else {
+  exit("Error!\n");
+}
