7946c86542b879de564d2bfcc927d53170a83911
Max Base <MaxBaseCode@Gmail.Com>
2020-11-06 17:45:33 +0330

    Improve crawler

--- importer.php
+++ importer.php
@@ -1,14 +1,22 @@
 <?php
-// Max Base
-// film2serial-api-service-crawler
+/*
+ * @Name: film2serial-api-service-crawler
+ * @Date: 2020-10-27, 2020-11-06
+ * @Version: 0.2
+ * @Repository: https://github.com/BaseMax/film2serial-api-service-crawler
+ */
+
+echo "The time is " . date("h:i:sa")."\n"; // We need to know execute time in log emails...
 ini_set('max_execution_time', 0);
 set_time_limit(0);
+
 require "NetPHP.php";
 require "phpedb.php";
+// use \Datetime;
 
 $db=new database();
-$db->connect("localhost", "yefilm_site", '****');
-$db->db="yefilm_site";
+$db->connect("localhost", "movie_db", 'df*gdf*gd*ff*gd*ff*gd*fg*dfg*');
+$db->db="movie_db";
 $db->create_database($db->db, false);
 
 class Film2Serial {
@@ -20,7 +28,7 @@ class Film2Serial {
 
 	public function parsePage($page=1, $input=null) {
 		if($input == null) {
-    	    print $this->linkHome()."page/" . $page."\n";
+    	   // print $this->linkHome()."page/" . $page."\n";
 			$input=get($this->linkHome()."page/" . $page)[0];
 		}
 		preg_match_all('/<h3><span class=\"icon\"><\/span>(\s*|)<a href=\"(?<value>[^\"]+)\" rel=\"bookmark\"/i', $input, $links);
@@ -32,7 +40,7 @@ class Film2Serial {
 	}
     
     function alt($text) {
-        return html_entity_decode(preg_replace('/([\-]{2,})/i', '-', preg_replace('/([\s\t\n]+)/i', '-', strip_tags($text))));
+        return trim(html_entity_decode(preg_replace('/([\-]{2,})/i', '-', preg_replace('/([\s\t\n]+)/i', '-', trim(strip_tags($text))))));
     }
     
 	public function parsePost($link, $input=null) {
@@ -46,6 +54,10 @@ class Film2Serial {
 		}
 		else {
 			return [];
+		}
+// 		if($db->count("sld_post", ["title"=>$title]) >= 1) {
+		if($db->count("sld_post", ["link"=>$link]) >= 1) {
+		    return [];
 		}
 		preg_match('/<div class="rightbox">(\s*|)<ul>(\s*|)<li><span class="icons daste"><\/span>(\s*|)(?<value>.*?)<\/li>/si', $input, $categories);
 		if(isset($categories["value"])) {
@@ -75,53 +87,84 @@ class Film2Serial {
 		else {
 		    $shortContext=$context;
 		}
-		foreach($categories as $i=>$category) {
-		    $category=trim($category);
-			$clauses=["name"=>$category];
-			if($db->count("sld_category", $clauses) == 0) {
-			    $clauses["keywords"]="";
-			    $clauses["fulldescr"]="";
-			    $clauses["alt_name"]=trim($this->alt($clauses["name"]));
-				$newID=$db->insert("sld_category", $clauses);
-				$categories[$i]=$newID;
+
+		preg_match('/<h2><span class="icon tags"><\/span> برچسب ها<\/h2>(\s*|)<\/div>(\s*|)<div class="contact">(\s*|)<h3>(?<value>.*?)<\/h3>(\s*|)<\/div>(\s*|)<\/div>/is', $input, $tag);
+        // print_r($tag);
+		if(isset($tag["value"])) {
+			$tag=$tag["value"];
+			preg_match_all('/rel="tag">(?<value>[^\<]+)<\/a>/si', $input, $tags);
+            // print_r($tags);
+			if(isset($tags["value"])) {
+    			$tags=$tags["value"];
+    			$tags=array_unique($tags);
+    			$tags=array_values($tags);
 			}
 			else {
-				$find=$db->select("sld_category", $clauses, "", "id");
-				if($find == null || $find == []) {
-					unset($categories[$i]);
-				}
-				else {
-					$categories[$i]=$find["id"];
-				}
+			    $tags=[];
 			}
 		}
-		print_r($categories);
-		if($categories != []) {
-			$categories=implode(",", $categories);
-		}
 		else {
-			$categories="";
+		    $tag="";
+		    $tags=[];
 		}
-		$data=[
+// 		print_r($tags);
+        $categoryFilter=[
+          "انیمیشن"=>"1",
+          "ایرانی"=>"2",
+          "سریال"=>"3",
+          "فیلم"=>"4",
+          "اکشن"=>"5",
+          "جنایی"=>"6",
+          "درام"=>"7",
+          "دوبله"=>"8",
+          "ترسناک"=>"9",
+          "هیجان انگیز"=>"10",
+          "خارجی"=>"11",
+          "زبان اصلی"=>"12",
+          "کمدی"=>"13",
+          "مستند"=>"14",
+          "دوبله فارسی"=>"15",
+          "خانوادگی"=>"16",
+          "فانتزی"=>"17",
+          "بیوگرافی"=>"18",
+          "جنگی"=>"19",
+          "رازآلود"=>"20",
+          "تاریخی"=>"21",
+          "موزیک"=>"22",
+          "ورزشی"=>"23",
+          "ماجرایی"=>"24",
+          "علمی تخیلی"=>"25",
+          "وسترن"=>"26",
+          "حیات وحش"=>"27",
+          "کلاسیک"=>"28",
+          "قدیمی دوبله"=>"29",
+          "موزیکال"=>"30",
+        ];
+        $now = new DateTime();
+        $data=[
+            "link"=>$link,
+		    "date"=>$now->format('Y-m-d H:i:s'),
 		    "xfields"=>"",
-			"autor"=>"admin-shop",
+			"autor"=>"admin",
 			"title"=>$title,
 // 			"alt_name"=>preg_replace('/([\s\t\n]+)/i', '', preg_replace('/([-]{2,})/i', '-', $title)),
 // 			"alt_name"=>preg_replace('/([\-]{2,})/i', '-', preg_replace('/([\s\t\n]+)/i', '', $title)),
 			"alt_name"=>$this->alt($title),
 			"short_story"=>$shortContext == null ? "" : $shortContext,
-			"descr"=>mb_substr(strip_tags($context), 0, 299, "utf-8"),
+			"descr"=>trim(preg_replace('/([\s\t\n]+)/i', ' ', mb_substr(strip_tags($context), 0, 299, "utf-8"))),
 // 			"descr"=>substr_replace(strip_tags($context), "...", 299),
 			"full_story"=>$context == null ? "" : $context,
-			"category"=>$categories,
+			"category"=>"",
 			"keywords"=>$keyword,
 			"approve"=>1,
 		];
-		if($db->count("sld_post", ["title"=>$title]) == 0) {
+		$clauses=["title"=>$title];
+		if($db->count("sld_post", $clauses) == 0) {
     		$postID=$db->insert("sld_post", $data);
 		}
 		else {
-		    $post=$db->select("sld_post", ["title"=>$title], "", "id");
+		    $post=$db->select("sld_post", $clauses, "", "id");
+		    $db->update("sld_post", $clauses, $data);
 		    $postID=$post["id"];
 		}
 		$_vals=[
@@ -150,15 +193,63 @@ class Film2Serial {
 		if($db->count("sld_post_extras", $clauses) == 0) {
 	        $db->insert("sld_post_extras", $_vals);
 		}
-		if($data["category"] != "") {
-		    foreach(explode(",", $data["category"]) as $category) {
-		        $clauses=["news_id"=>$postID, "cat_id"=>$category];
-		        print_r($clauses);
-        		if($db->count("sld_post_extras_cats", $clauses) == 0) {
-    		        $db->insert("sld_post_extras_cats", $clauses);
-        		}
+
+        // category
+// 		print_r($categories);
+// 		$_cats=[];
+		foreach($categories as $i=>$category) {
+		    $category=trim($category);
+		  //  if(isset($categoryFilter[$category])) {
+    // 	        $_cats[]=$categoryFilter[$category];
+		  //  }
+			$clauses=["name"=>$category];
+			if($db->count("sld_category", $clauses) == 0) {
+			    $clauses["keywords"]="";
+			    $clauses["fulldescr"]="";
+			    $clauses["alt_name"]=trim($this->alt($clauses["name"]));
+				$newID=$db->insert("sld_category", $clauses);
+			}
+			else {
+				$find=$db->select("sld_category", $clauses, "", "id");
+				if($find == null || $find == []) {
+					unset($categories[$i]);
+					continue;
+				}
+				else {
+				    $newID=$find["id"];
+				}
+			}
+			$db->delete("sld_post_extras_cats", ["news_id"=>$postID]);
+	        $extcatID=$db->insert("sld_post_extras_cats", ["news_id"=>$postID, "cat_id"=>$newID]);
+	       // $_cats[]=$extcatID;
+			$_cats[]=$newID;
+		}
+// 		print "--------\n";
+// 		print_r($_cats);
+		if($_cats !=[]) {
+		    $_cats=implode(",", $_cats);
+    		$db->update("sld_post", ["id"=>$postID], ["category"=>$_cats]);
+		}
+		else {
+    		$db->update("sld_post", ["id"=>$postID], ["category"=>""]);
+		}
+        
+        // print_r($tags);
+	    $clauses=["news_id"=>$postID];
+	    $db->delete("sld_tags", $clauses);
+		foreach($tags as $item) {
+		    $clauses=["news_id"=>$postID, "tag"=>$item];
+		  //  print_r($clauses);
+		    if($db->count("sld_tags", $clauses) == 0) {
+    		    $db->insert("sld_tags", $clauses);
 		    }
 		}
+		if($tags != []) {
+		    $stringTags=implode(",", $tags);
+		    print $postID."\n";
+		    $db->update("sld_post", ["id"=>$postID], ["tags"=>$stringTags]);
+		}
+
 		return $data;
 	}
 
@@ -172,27 +263,48 @@ class Film2Serial {
 			$lastPage=(int)$lastPage["value"];
 			return $lastPage;
 		}
-	}	
+	}
 }
 $service=new Film2Serial();
+
+// Testing
+// $post=$service->parsePost("https://www.film2serial.ir/1399/08/10/%d8%af%d8%a7%d9%86%d9%84%d9%88%d8%af-%d9%81%d8%b5%d9%84-%d8%a7%d9%88%d9%84-%d8%b3%d8%b1%db%8c%d8%a7%d9%84-dracula-2020-%d8%a8%d8%a7-%d8%af%d9%88%d8%a8%d9%84%d9%87-%d9%81%d8%a7%d8%b1%d8%b3%db%8c.html");
+// exit();
+
 $input=get($service->linkHome())[0];
 $page=$service->countPage($input);
 print "Page: ".$page."\n";
 //$page=10;
 //$page=1;
-for($i=1;$i<=$page;$i++) {
+// $i=1;
+// $i=$page/2;
+$i=$page;
+// $i=$page-30;
+// $i=35;
+// $i=180;
+// $i=$page-150;
+// $i=$page-220;
+// $i=$page-250;
+// $i=$page-440;
+// $i=300;
+// $i=400;
+// for(;$i<=$page;$i++) {
+for(;$i>=1;$i--) {
     if($i == 1) {
     	$links=$service->parsePage($i, $input);
     }
     else {
     	$links=$service->parsePage($i);
     }
-	print_r($links);
+// 	print_r($links);
 	foreach($links as $link) {
 		$post=$service->parsePost($link);
-		print_r($post);
+// 		print_r($post);
+        print '.';
 	}
+	print '#';
 }
+print "\nDone.";
 /*
 INSERT INTO `sld_category` (`id`, `parentid`, `posi`, `name`, `alt_name`, `icon`, `skin`, `descr`, `keywords`, `news_sort`, `news_msort`, `news_number`, `short_tpl`, `full_tpl`, `metatitle`, `show_sub`, `allow_rss`, `fulldescr`, `disable_search`, `disable_main`, `disable_rating`, `disable_comments`, `enable_dzen`, `enable_turbo`, `active`, `rating_type`) VALUES (NULL, '0', '1', 'خبرها', 'news', '', '', '', '', '', '', '0', '', '', '', '0', '1', '', '0', '0', '0', '0', '1', '1', '1', '-1')
 
